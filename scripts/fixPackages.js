9405e4015d2496dd4444b36efb9d4f2a23a5ccf5
Denis Ermolin <denis.ermolin@matic.network>
2021-09-16 18:08:58 +0700

    fix: correct versions & replacement

--- scripts/fixPackages.js
+++ scripts/fixPackages.js
@@ -5,19 +5,27 @@ const fs = require('fs');
 const path = require('path');
 
 const A_FIX = 'npptesttypes/build';
-const TO_FIX = '@polkadot/types';
+const TO_FIX = ['@polkadot/types', '@polkadot/metadata/node_modules/@polkadot/types'];
 
 function copyFiles (source, dest) {
+  if (!fs.existsSync(dest)) {
+    fs.mkdirSync(dest, { recursive: true });
+  }
+
   fs.readdirSync(source).forEach((entry) => {
     const resourcePathSource = path.join(source, entry);
     const resourcePathDest = path.join(dest, entry);
 
     if (fs.lstatSync(resourcePathSource).isDirectory()) {
+      if (!fs.existsSync(resourcePathDest)) {
+        fs.mkdirSync(resourcePathDest, { recursive: true });
+      }
+
       copyFiles(resourcePathSource, resourcePathDest);
     } else {
       const sourceData = fs.readFileSync(resourcePathSource);
 
-      fs.writeFileSync(resourcePathDest, sourceData);
+      fs.writeFileSync(resourcePathDest, sourceData, { flag: 'wx' });
     }
   });
 }
@@ -25,7 +33,10 @@ function copyFiles (source, dest) {
 function findPackages () {
   const nodeModulesRoot = path.join(__dirname, '..', 'node_modules');
 
-  copyFiles(path.join(nodeModulesRoot, A_FIX), path.join(nodeModulesRoot, TO_FIX));
+  for (const p of TO_FIX) {
+    fs.rmdirSync(path.join(nodeModulesRoot, p), { force: true, recursive: true });
+    copyFiles(path.join(nodeModulesRoot, A_FIX), path.join(nodeModulesRoot, p));
+  }
 }
 
 findPackages();
