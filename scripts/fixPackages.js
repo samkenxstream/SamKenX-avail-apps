2748a4d0009c19790be3ae3f0bdb3236a90e7925
Denis Ermolin <denis.ermolin@matic.network>
2021-09-17 17:01:20 +0700

    fix: build

--- scripts/fixPackages.js
+++ scripts/fixPackages.js
@@ -5,7 +5,12 @@ const fs = require('fs');
 const path = require('path');
 
 const A_FIX = 'npptesttypes/build';
-const TO_FIX = ['@polkadot/types', '@polkadot/metadata/node_modules/@polkadot/types'];
+const TO_FIX = [
+  '@polkadot/types',
+  '@polkadot/metadata/node_modules/@polkadot/types',
+  'moonbeam-types-bundle/node_modules/@polkadot/types',
+  '@polkadot/types-known/node_modules/@polkadot/types'
+];
 
 function copyFiles (source, dest) {
   if (!fs.existsSync(dest)) {
